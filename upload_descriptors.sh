8ab57a5a27a0afebf6ca4c0a3bd04c0121a187ae
Josep Batallé <jbatalle3@gmail.com>
2016-12-12 16:06:43 +0100

    Updated upload vnf script and vnfs format

--- upload_descriptors.sh
+++ upload_descriptors.sh
@@ -2,37 +2,49 @@
 import json
 import requests
 
-url = 'http://10.10.1.90/auth/'
-payload = {"username":"admin","password":"123456"}
-headers = {'content-type': 'application/json'}
-response = requests.post(url, data=json.dumps(payload), headers=headers)
-token = response.json()['token']
-
 #list of descriptors:
 descriptors = [
 	{"type": "proxy", "vnfd": "vnfds/vproxy.json", "nsd": "nsds/vproxy.json"},
-	{"type": "vtc", "vnfd": "vnfds/vtc.json", "nsd": "nsds/vtc.json"},
 	{"type": "vsbc", "vnfd": "vnfds/vsbc.json", "nsd": "nsds/vsbc.json"},
 	{"type": "vhg", "vnfd": "vnfds/vhg.json", "nsd": "nsds/vhg.json"},
+	{"type": "vtc", "vnfd": "vnfds/vtc.json", "nsd": "nsds/vtc.json"}
 ]
 
-url = 'http://10.10.1.90/vnfs/'
-headers = {'content-type': 'application/json', 'Authorization': 'JWT ' + token}
+def auth(user):
+	url = 'http://10.10.1.90/auth/'
+	payload = {"username": user,"password":"123456"}
+	headers = {'content-type': 'application/json'}
+	response = requests.post(url, data=json.dumps(payload), headers=headers)
+	token = response.json()['token']
+	return token
+
 
 for descriptor in descriptors:
+
+	token = auth("fp1")
+	headers = {'content-type': 'application/json', 'Authorization': 'JWT ' + token}
+	url = 'http://10.10.1.90/vnfs/'
 
 	#upload VNFD to Marketplace
 	response = requests.post(url, data=open(descriptor['vnfd'], 'rb'), headers=headers)
 	vnfd = response.json()
+
+	print "New VNFD: %d" % vnfd['id']
 
 	#replace id in NSD
 	file = open(descriptor['nsd'])
 	contents = file.read()
 	replaced_contents = contents.replace('<updated_id>', str(vnfd['id']))
 
+	token = auth("sp1")
+	headers = {'content-type': 'application/json', 'Authorization': 'JWT ' + token}
+
 	#upload NSD to Marketplace
 	url = 'http://10.10.1.90/service-catalog/service/catalog/'
 	response = requests.post(url, data=replaced_contents, headers=headers)
+	nsd = response.json()
+
+	print "New NSD: %s" % nsd['nsd']['id']
 
 print "Done."
 
